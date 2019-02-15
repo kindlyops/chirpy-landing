c008fe83623ea14616df1f9d25eac9c17398c616
Elliot Murphy <statik@users.noreply.github.com>
2019-02-15 15:06:15 -0500

    hugo install script
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

---
+++ ci-install-hugo.sh
@@ -0,0 +1,11 @@
+HUGO_VERSION=0.54.0
+HUGO_DOWNLOAD=hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+
+set -x
+set -e
+
+# Install Hugo if not already cached or upgrade an old version.
+if [ ! -e hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
+  tar xvzf ${HUGO_DOWNLOAD}
+fi
