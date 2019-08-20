f7feca73365da226ad26f001f848ccfa82883efe
Elliot Murphy <statik@users.noreply.github.com>
2019-08-20 17:34:26 -0400

    shellcheck cleanup
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 HUGO_VERSION=0.57.2
 HUGO_DOWNLOAD=hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 
@@ -5,7 +7,7 @@ set -x
 set -e
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+if [ ! -e hugo ] || ! [[ $(hugo version) =~ v${HUGO_VERSION} ]]; then
   wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
   tar xvzf ${HUGO_DOWNLOAD}
 fi
